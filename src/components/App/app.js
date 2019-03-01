6a40cd7347f6f9ccd2f5c31b74ac75dd4d5f58a4
Postnov Daniil <dan.postnov@gmail.com>
2019-03-01 14:47:21 +0300

    add search

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -5,12 +5,14 @@ import Search from '../Search';
 import cars from '../../assets/cars';
 
 import './app.css';
+import { all } from 'q';
 export default class App extends Component {
 
 
   state = {
     cars: cars || {},
     userCoords: ['55.7536232', '37.6199775'],
+    filteredCars: cars
   };
 
   setDialerDistance = (distance, carId) => {
@@ -28,23 +30,45 @@ export default class App extends Component {
 
   switchSearch = (e) => {
     let filter = e.target.value;
-    let newCars = cars.slice().sort((a,b) => a[filter] - b[filter]);
+    let filteredCars = cars.slice().sort((a,b) => a[filter] - b[filter]);
 
-    this.setState({cars: newCars});
+    this.setState({filteredCars});
   };
 
+  onSearch = (e) => {
+    let searchQuery = e.target.value;
+
+    let filteredCars = cars.filter(el => {
+      let {
+        model_name:modelName = '',
+        kit_name:kitName = '',
+        dealer: {
+          address,
+          city,
+          name
+        }
+      } = el;
+
+      let allValues = [modelName, kitName, address, city, name].join(' ');
+
+      return allValues.toLowerCase().indexOf(searchQuery.toLowerCase) !== -1;
+    });
+
+    // this.setState({filteredCars});
+  }
+
   render() {
-    let {cars, userCoords} = this.state;
+    let {filteredCars, userCoords} = this.state;
     return (
       <div className="app">
 
         <div className="app__top-panel">
           <Sort switchSearch={this.switchSearch}/>
-          <Search />
+          <Search onSearch={this.onSearch}/>
         </div>
 
         <ListAuto
-          items={cars}
+          items={filteredCars}
           userCoords={userCoords}
           setDialerDistance={this.setDialerDistance}/>
       </div>
--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 
 import './search.css';
 
-const Search = () => {
+const Search = ({onSearch}) => {
     return (
       <div class="search">
         <input
           type="text"
           class="search__input"
+          onChange={onSearch}
+          defaultValue=""
           placeholder="Поиск по авто, названию, адресу, городу филиала"/>
       </div>
     )

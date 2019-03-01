40651df5a1f063559256eb00010c3cb9ae9ee00e
Postnov Daniil <dan.postnov@gmail.com>
2019-03-01 11:37:07 +0300

    add all data in auto component

--- src/components/Auto/auto.js
+++ src/components/Auto/auto.js
@@ -5,44 +5,110 @@ import './auto.css';
 
 export default class Auto extends Component {
 
+  state = {
+    showAll: false
+  }
+
+  setShowFeatures = (showState) => {
+    this.setState({
+      showAll: showState
+    });
+  };
+
+
+
   render() {
+    let {showAll} = this.state;
+    let {
+      car: {
+        model_name:modelName,
+        kit_name:kitName = false,
+        images: [carImg],
+        price: carPrice,
+        features: carFeatures,
+        dealer: {
+          name:dealerName,
+          city: dealerCity,
+          address:dealerStreet,
+          latitude:dLat = null,
+          longitude:dLon = null,
+          url:dealerUrl
+        } = {},
+      },
+      userCoords: [userLat, userLon]
+
+    } = this.props;
+
+    //distance for dealder
+    let distance = 0;
+    if (dLat, dLon) distance = getDistance(dLat,dLon,userLat,userLon).toFixed(1) + ' км. ';
+
+    // form dealer adress
+    let dealerAddress = '';
+
+    if (distance) dealerAddress += distance;
+    if (dealerName) dealerAddress += dealerName + ', ';
+    if (dealerCity) dealerAddress += dealerCity + ', ';
+    if (dealerStreet) dealerAddress += dealerStreet + '';
+
+    // features and set count showing
+    let countFeatures = showAll ? carFeatures.length : 3;
+
+    let features = carFeatures.slice(0, countFeatures).map((el, index) => {
+      return (
+        <li className="auto__feature" key={el + index}>{el}</li>
+      );
+    });
+
+    //button for show/hide features
+    let btnManageShowList;
+
+    if (showAll) {
+      btnManageShowList = <span className="auto__show-more" onClick={() => this.setShowFeatures(false)}>скрыть</span>;
+    }else {
+      btnManageShowList = <span className="auto__show-more" onClick={() => this.setShowFeatures(true)}> ещe {carFeatures.length - 3} особенностей</span>;
+    };
+
+    //dealder link
+    let dealerLink;
+
+    if (dealerUrl) {
+      dealerLink = <a target="_blank" href="#" className="auto__address"><span>{dealerAddress}</span></a>;
+    }
+    else dealerLink = <span className="auto__address">{dealerAddress}</span>;
+
+
+
 
     return (
       <div className="item-auto auto">
         <header className="auto__header">
           <img
-            src="car.images"
-            alt="`Изображение авто:`"
+            src={carImg}
+            alt={`Изображение авто: ${carImg}`}
             className="auto__img" />
 
-          <p className="auto__title"></p>
-          <p className="auto__price">₽</p>
+          <p className="auto__title">{modelName + kitName}</p>
+          <p className="auto__price">{carPrice.toLocaleString()} ₽</p>
         </header>
         <div className="auto__body">
+          {features ? <ul className="auto__features"> {features} </ul> : null}
 
-            <ul className="auto__features">
-                <li className="auto__feature"></li>
-            </ul>
-
-            <span className="auto__show-more">еще 27 особенностей</span>
-            <span className="auto__show-more">скрыть</span>
+          {carFeatures.length > 3 ? btnManageShowList : null}
         </div>
 
         <div className="auto__footer">
-            <svg className="auto__loc-icon" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
-              <g fillRule="evenodd">
-                <path d="m249.1 82c54.4 0 113.1 33.7 114.9 124.2 0
-                78.4-88.8 175.8-109.7 198.5-1.7 1.8-6.9 1.8-8.7
-                0-20.8-20.9-109.6-118.4-109.6-198.5 0-90.5 57.8-124.2
-                113.1-124.2zm0.8 148.8c19.9 0 36-16.1 36-35.9s-16.1-35.9-36-35.9-36
-                16.1-36 35.9 16.1 35.9 36 35.9z"/>
-              </g>
-            </svg>
-            <a target="_blank" href="#" className="auto__address">
-                <span>Адрес дилера</span>
-            </a>
-            <span className="auto__address">Адрес дилера</span>
+          <svg className="auto__loc-icon" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+            <g fillRule="evenodd">
+              <path d="m249.1 82c54.4 0 113.1 33.7 114.9 124.2 0
+              78.4-88.8 175.8-109.7 198.5-1.7 1.8-6.9 1.8-8.7
+              0-20.8-20.9-109.6-118.4-109.6-198.5 0-90.5 57.8-124.2
+              113.1-124.2zm0.8 148.8c19.9 0 36-16.1 36-35.9s-16.1-35.9-36-35.9-36
+              16.1-36 35.9 16.1 35.9 36 35.9z"/>
+            </g>
+          </svg>
 
+          {dealerLink}
         </div>
     </div>
     );

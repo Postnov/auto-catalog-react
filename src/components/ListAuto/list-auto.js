dc112e09d02f558e91d8e5f7663c073fd1023303
Postnov Daniil <dan.postnov@gmail.com>
2019-03-01 12:59:35 +0300

    add set the same height on list item

--- src/components/ListAuto/list-auto.js
+++ src/components/ListAuto/list-auto.js
@@ -4,12 +4,35 @@ import Masonry from 'react-masonry-css';
 
 import './list-auto.css';
 
-const ListAuto = ({items,userCoords,setDialerDistance}) => {
+export default class ListAuto extends Component {
 
+  state = {
+    minHeight: 0
+  }
+
+  componentDidMount() {
+    window.addEventListener('load', () => {
+      let listItems = Object.values(this.refs),
+          arrHeight = listItems.map(el => el.offsetHeight),
+          minHeight = Math.max.apply(null, arrHeight);
+
+      this.setState({minHeight})
+    });
+
+  }
+
+  render() {
+    let {items,userCoords,setDialerDistance} = this.props;
+    let {minHeight} = this.state;
 
     const cars = items.map(el => {
       return (
-        <div className="list-auto__item" key={el.id}>
+        <div
+          className="list-auto__item"
+          key={el.id}
+          ref={'auto'+ el.id}
+          style={{minHeight: minHeight + 'px'}}>
+
           <Auto
             car={el}
             userCoords={userCoords}
@@ -26,6 +49,6 @@ const ListAuto = ({items,userCoords,setDialerDistance}) => {
         {cars}
       </Masonry>
     )
-}
+  }
 
-export default ListAuto;
+}

008119441d8026258cf3355468c9154a199d4808
Postnov Daniil <dan.postnov@gmail.com>
2019-03-01 10:08:05 +0300

    add compound

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -2,7 +2,20 @@ import React, {Component} from 'react';
 import ListAuto from '../ListAuto';
 import Sort from '../Sort';
 import Search from '../Search';
+import cars from '../../assets/cars';
 
 export default class App extends Component {
 
+
+  state = {
+    cars: cars || {}
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <ListAuto items={cars}/>
+      </div>
+    )
+  }
 }

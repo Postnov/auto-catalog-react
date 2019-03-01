fe91365ac6904fc9988c4e1023d89b9a10fe1d56
Postnov Daniil <dan.postnov@gmail.com>
2019-03-01 10:10:21 +0300

    change few attributes

--- src/components/Auto/auto.js
+++ src/components/Auto/auto.js
@@ -8,10 +8,9 @@ export default class Auto extends Component {
   render() {
 
     return (
-      <div className="item-auto auto" v-if="car">
+      <div className="item-auto auto">
         <header className="auto__header">
           <img
-            v-if="car.images"
             src="car.images"
             alt="`Изображение авто:`"
             className="auto__img" />
@@ -31,7 +30,7 @@ export default class Auto extends Component {
 
         <div className="auto__footer">
             <svg className="auto__loc-icon" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
-              <g fill-rule="evenodd">
+              <g fillRule="evenodd">
                 <path d="m249.1 82c54.4 0 113.1 33.7 114.9 124.2 0
                 78.4-88.8 175.8-109.7 198.5-1.7 1.8-6.9 1.8-8.7
                 0-20.8-20.9-109.6-118.4-109.6-198.5 0-90.5 57.8-124.2
@@ -42,7 +41,7 @@ export default class Auto extends Component {
             <a target="_blank" href="#" className="auto__address">
                 <span>Адрес дилера</span>
             </a>
-            <span v-else className="auto__address">Адрес дилера</span>
+            <span className="auto__address">Адрес дилера</span>
 
         </div>
     </div>
